e80f73c9a198f67a075e8b29988e93478be92e23
Ayumi267 <1224521@ed.tus.ac.jp>
2025-03-01 12:30:14 +0900

    Update up+bottom_absorption_depth_ver3.py

--- up+bottom_absorption_depth_ver3.py
+++ up+bottom_absorption_depth_ver3.py
@@ -10,7 +10,7 @@ from scipy.signal import find_peaks
 import copy
 
 # about not irradiate
-Google_Drive_datadir = "/Users/kobayashiaiketsu/Library/CloudStorage/GoogleDrive-a.kobayashi267@gmail.com/.shortcut-targets-by-id/1ZkGf0KEeIpXHgpjgZ6RIBuECFYX3dERR/study/data/experiment/spectra/kobayashi/NaCl/"
+Google_Drive_datadir = "/path/to/data/experiment/spectra/kobayashi/NaCl/"
 DATE = "2022-03-18"
 FILE_ID_IRRADIATED_UP = "16"
 DATA_FILE_PATH_IRRADIATED_UP = f"{Google_Drive_datadir}/{DATE}/{DATE}_{FILE_ID_IRRADIATED_UP}.txt"
